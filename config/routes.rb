079ecbfe2390d5af11e6bb0f512a54475b6ec6f6
tommaso <raspaccio@gmail.com>
2013-10-27 21:11:39 +0100

    adds bootstrap 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Pinteresting::Application.routes.draw do
-
   root 'pages#home'
-
-  get 'about' => 'pages#about'
+  get 'about' => 'pages#about' # creates the about_path
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
